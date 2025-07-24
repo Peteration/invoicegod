61311d47cbc06c03bc1450fd899a2c37343f536e
Peteration <chriswyatt045@gmail.com>
2025-07-24 17:03:07 +0100

    Reapplied local changes after sync

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react'; 
+export default function Home() { 
+    <div className="container"> 
+      <h1>Invoice Generator</h1> 
+      <p>Welcome to your invoice application</p> 
+    </div> 
+  ); 
+} 
